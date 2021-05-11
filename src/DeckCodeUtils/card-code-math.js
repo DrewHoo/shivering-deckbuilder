5d65a0b6531964107cf656b58b78dbfec24ac91a
DrewHoo <drew@ripplescience.com>
2021-05-11 14:16:39 -0400

    fix: fix deck code bug. add missing images

--- src/DeckCodeUtils/card-code-math.js
+++ src/DeckCodeUtils/card-code-math.js
@@ -1,3 +1,5 @@
+// this only works for 2-digit base 26 numbers that start at A
+// which is what deckcodes and collection codes are made of
 export function turnNumberIntoBase26 (number) {
   let i = 0
   while (number > 26) {
@@ -5,4 +7,10 @@ export function turnNumberIntoBase26 (number) {
     i++
   }
   return String.fromCharCode(65 + i) + String.fromCharCode(65 + number)
+}
+
+export function turnBase26IntoNumber (base26Number) {
+  return (
+    26 * (base26Number.charCodeAt(0) - 65) + (base26Number.charCodeAt(1) - 65)
+  )
 }
--- src/DeckCodeUtils/deck-code-mapping.js
+++ src/DeckCodeUtils/deck-code-mapping.js
@@ -1,34 +1,40 @@
 import _ from 'lodash'
 import { cards as collection } from '../collection'
-import { turnNumberIntoBase26 } from './card-code-math'
+import { turnBase26IntoNumber, turnNumberIntoBase26 } from './card-code-math'
 
 export function getCardsFromDeckCode (deckCode) {
-  let count = 1
+  let index = 0
+  let count = 0
   return _.chunk(deckCode.slice(2), 2)
     .map(arr => arr.join(''))
     .flatMap(cardCode => {
-      const card = collection.find(({ code }) => code === cardCode)
-      if (card) {
-        return _.range(1, count).map(() => card)
-      } else {
+      if (index === 0) {
+        index = turnBase26IntoNumber(cardCode)
         count += 1
         return []
+      } else {
+        const card = collection.find(({ code }) => code === cardCode)
+        index -= 1
+        return _.range(1, count + 1).map(() => card)
       }
     })
     .filter(card => !!card)
 }
 
 export function deckCodeToCardList (deckCode) {
+  let index = 0
   let count = 0
   return _.chunk(deckCode.slice(2), 2)
     .map(arr => arr.join(''))
     .map(code => {
-      const card = collection.find(card => card.code === code)
-      if (card) {
-        return { ...card, count }
-      } else {
+      if (index === 0) {
+        index = turnBase26IntoNumber(code)
         count += 1
         return undefined
+      } else {
+        const card = collection.find(card => card.code === code)
+        index -= 1
+        return { ...card, count }
       }
     })
     .filter(obj => !!obj)
@@ -43,15 +49,22 @@ export function deckCodeToCardList (deckCode) {
  */
 export function turnDeckCodeToObject (deckCode) {
   let count = 0
+  let index = 0
   return _.chunk(deckCode.slice(2), 2)
     .map(arr => arr.join(''))
     .map(code => {
-      const card = collection.find(card => card.code === code)
-      if (card) {
-        return [count, card.code]
-      } else {
+      if (index === 0) {
+        index = turnBase26IntoNumber(code)
         count += 1
         return [count]
+      } else {
+        index -= 1
+        const card = collection.find(card => card.code === code)
+        if (!card) {
+          debugger;
+          console.log(code)
+        }
+        return [count, card.code]
       }
     })
     .reduce(
